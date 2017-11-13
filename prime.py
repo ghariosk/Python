782462f628d7d740088ef9b0d1e26be9f7e17a76
Karl Gharios <karlgharios@Karls-MBP.lan>
2017-11-13 07:55:58 +0000

    prime.py

---
+++ prime.py
@@ -0,0 +1,47 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+def prime (n):
+	i=1
+	array=[1]
+	hello = [1]
+	prime = []
+	while i <= n:
+		b = i 
+		prime=[]
+		while b > 0:
+			sol = i % b
+			if sol == 0 and i != b:
+				array.append(i)
+			b -= 1
+		i += 1
+	i = 0
+	ratio = 0 
+	while i < len(array):
+		if array.count(i) == 1:
+			print (i)
+		# 	hello.append(1)
+		# else :
+		# 	hello.append(0)
+
+		# prime = hello.append(hello.count(1)/len(hello))
+		i += 1
+	# return prime
+	
+	
+
+print ('This code will output a list of prime numbers until the number you entered.')
+a= int(input ('Please enter a number : '))
+print ('The list of prime numbers up to this number is :')
+prime (a)
+
+# print (prime)
+
+# plt.plot(prime,)
+
+# plt.show()
+
+
+
+ 
+
---
+++ time.py
@@ -0,0 +1,82 @@
+import datetime as dt
+
+a = dt.datetime.now()
+
+print (a)
+a = str(a)
+# a= a[11:16]
+
+time=a[11:13]+a[14:16]
+
+
+print (time)
+
+#time=int(input("Please enter the time: "))
+#string=str(time)
+
+string=a[11:13]+a[14:16]
+time = int(string)
+finished=False
+word = "You entered a wrong time"
+while word == 'You entered a wrong time':
+        if word == 'You entered a wrong time':
+                word = '.'
+                if len(string) == 3:
+                        string = string[0:1]+string[1:]
+                        a = int(string[1:])
+                        if a>59:
+                            word = 'You entered a wrong time'
+                            time = int(input("You entered a wrong time \n Please enter the time: "))
+                        elif time > 400 and time < 1000:
+                            word = ","
+                                                  
+                            print('Good morning!')
+                            
+                             
+                elif len(string)==4:
+                        string=string[0:2]+string[2:]
+                        a=int(string[2:])
+                        if a>59:
+                            word='You entered a wrong time'
+                            time=int(input("you entered a wrong time \n Please enter the time: "))
+                        elif time>=1000 and time<1100:
+                            print('Good morning!')
+                        elif time>=1100 and time<1300:
+                            print('Good day!')
+                        elif time>=1300 and time<1800:
+                            print('Good afternoon!')
+                        elif time>=1800 and time<2300:
+                            print('Good evening!')
+                        else:
+                            print('Good night!')
+                elif len(string)==2 and time>0:
+                    
+                        a=int(string)
+                        if a>59:
+                            print('You entered a wrong time')
+                        else:    
+                            print('Good night!')
+                elif len(string)==1 and time>0:
+                        a=int(string)
+                        print('Good night!')
+                else:
+                        time=int(input('You entered a wrong time'))
+
+def Hello(a):
+
+	print((a**2)/3)
+
+a = [1,2,3,4,5,6,7,8,5,43,2,23,5,436,12312,42,654]
+print(a)
+
+
+b = list(map(lambda x: Hello(x) , a))
+
+
+# b = map (Hello(a), a)
+
+# print (b)
+
+
+
+
